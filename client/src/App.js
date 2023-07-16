658e3c0b0a51040eda9716e70935bed49a955195
Aryan Raj <aryanraj2k25@gmail.com>
2023-07-16 12:48:11 +0530

    main page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,59 @@
+import React from 'react';
+import { Typography, Box, Button } from '@mui/material';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <Box
+    //can add background here, just need to modify app-container a bit
+      //className="app-container"
+      sx={{
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '30vh',
+      }}
+    >
+      <Typography variant="h1" align="center" sx={{ marginTop: '5rem' }}>
+        ByteBit
+      </Typography>
+      <Typography variant="h4" align="center" sx={{ marginTop: '2rem' }}>
+        A simple file compression tool
+      </Typography>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: '2rem',
+        }}
+      >
+        <div className="media-clipper" />
+        {/**Compress Button */}
+        <Box sx={{ marginTop: '1rem' }}>
+          <Button 
+          variant="contained" 
+          color="primary" 
+          component={Link}
+          to="/compressed"
+          sx={{ marginRight: '20px' }}>
+            Compress
+          </Button>
+          {/**Decompress Button */}
+          <Button variant="contained" 
+          color="primary"
+          component={Link}
+          to="/decompressed">
+            Decompress
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My App
+        <Typography variant="h6" component={Link} to="/" sx={{ flexGrow: 1 }} >
+          ByteBit
         </Typography>
-        <Button color="inherit">Home</Button>
+        {/**Phase 2 Development */}
+
+        {/* <Button color="inherit">Help</Button>
         <Button color="inherit">About</Button>
-        <Button color="inherit">Contact</Button>
+        <Button color="inherit">Contact</Button> */}
       </Toolbar>
     </AppBar>
   );
