3f5ae7f3642646640ecfc3bf902f31e7dcf8b052
Aryan Raj <aryanraj2k25@gmail.com>
2023-07-27 16:31:20 +0530

    Media Button added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Typography, Box, Button } from '@mui/material';
+import { Typography, Box, Button, Divider} from '@mui/material';
 import './App.css';
 import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <Box
-    //can add background here, just need to modify app-container a bit
+      //can add background here, just need to modify app-container a bit
       //className="app-container"
       sx={{
         backgroundColor: 'rgba(255, 255, 255, 0.5)',
@@ -24,35 +24,22 @@ function App() {
       <Typography variant="h4" align="center" sx={{ marginTop: '2rem' }}>
         A simple file compression tool
       </Typography>
+      <Divider></Divider>
 
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          marginTop: '2rem',
-        }}
-      >
-        <div className="media-clipper" />
-        {/**Compress Button */}
-        <Box sx={{ marginTop: '1rem' }}>
-          <Button 
-          variant="contained" 
-          color="primary" 
-          component={Link}
-          to="/compressed"
-          sx={{ marginRight: '20px' }}>
-            Compress
-          </Button>
-          {/**Decompress Button */}
-          <Button variant="contained" 
-          color="primary"
-          component={Link}
-          to="/decompressed">
-            Decompress
-          </Button>
-        </Box>
-      </Box>
+      <Button startDecorator>Add Media</Button>
+      <Typography variant="h12" align="center" sx={{ marginTop: '2rem' }}>Add your media file, in .txt, .docx format only</Typography>
+
+      <Box sx={{ marginTop: '1rem', display: 'flex', alignItems: 'center' }}>
+      
+       <Button variant="contained" color="primary" component={Link} to="/compressed" sx={{ marginRight: '20px' }}>
+        Compress
+        </Button>
+        {/* Decompress Button */}
+        <Button variant="contained" color="primary" component={Link} to="/decompressed">
+        Decompress
+        </Button>
+</Box>
+
     </Box>
   );
 }
