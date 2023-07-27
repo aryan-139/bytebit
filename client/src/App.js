efc9728e8ffe2a395f592dad60302a89eb83cca3
Aryan Raj <aryanraj2k25@gmail.com>
2023-07-27 22:54:42 +0530

    file input frontend complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import { Typography, Box, Button, Divider} from '@mui/material';
+import React, { useRef, useState } from 'react';
+import { Typography, Box, Button } from '@mui/material';
 import './App.css';
 import { Link } from 'react-router-dom';
 
 function App() {
+  const fileInputRef = useRef(null);
+  const [mediaAdded, setMediaAdded]=useState(false);
+
+  const handleFileInputChange = async (e) => {
+    const file = e.target.files[0];
+
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+      //for testing only 
+      setMediaAdded(true);
+
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        // File uploaded successfully
+        setMediaAdded(true);
+        console.log('File uploaded successfully');
+      } else {
+        // Error occurred while uploading the file
+        console.error('Error uploading file');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleButtonClick = () => {
+    // Trigger the hidden input element when the button is clicked
+    fileInputRef.current.click();
+  };
+
   return (
     <Box
-      //can add background here, just need to modify app-container a bit
-      //className="app-container"
       sx={{
         backgroundColor: 'rgba(255, 255, 255, 0.5)',
         padding: '2rem',
@@ -18,28 +51,57 @@ function App() {
         height: '30vh',
       }}
     >
-      <Typography variant="h1" align="center" sx={{ marginTop: '5rem' }}>
-        ByteBit
-      </Typography>
-      <Typography variant="h4" align="center" sx={{ marginTop: '2rem' }}>
-        A simple file compression tool
-      </Typography>
-      <Divider></Divider>
+      <div id='header'>
+        <Typography variant="h1" align="center" sx={{ marginTop: '5rem' }}>
+          ByteBit
+        </Typography>
+        <Typography variant="h4" align="center" sx={{ marginTop: '2rem' }}>
+          A simple file compression tool
+        </Typography>
+      </div>
 
-      <Button startDecorator>Add Media</Button>
-      <Typography variant="h12" align="center" sx={{ marginTop: '2rem' }}>Add your media file, in .txt, .docx format only</Typography>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        onChange={handleFileInputChange}
+      />
+
+      <Button startDecorator onClick={handleButtonClick}>
+        Add Media
+      </Button>
+      {/**Display that the media is added successfully and a TICK MARK */}
+      <div id='mediaAdded'>
+        {mediaAdded && (
+          <Typography variant="h12" color="green" align="center" sx={{ marginTop: '2rem' }}>
+            Media added successfully
+          </Typography>
+        )}
+      </div>
+
+      <Typography variant="h12" align="center" sx={{ marginTop: '2rem' }}>
+        Add your media file, in .txt, .docx format only
+      </Typography>
 
       <Box sx={{ marginTop: '1rem', display: 'flex', alignItems: 'center' }}>
-      
-       <Button variant="contained" color="primary" component={Link} to="/compressed" sx={{ marginRight: '20px' }}>
-        Compress
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to="/compressed"
+          sx={{ marginRight: '20px' }}
+        >
+          Compress
         </Button>
-        {/* Decompress Button */}
-        <Button variant="contained" color="primary" component={Link} to="/decompressed">
-        Decompress
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to="/decompressed"
+        >
+          Decompress
         </Button>
-</Box>
-
+      </Box>
     </Box>
   );
 }
---
+++ server/routes/upload.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  // Implement media upload logic here
+  // Store the media in a middleware to be used later for compression
+  // Respond with success message or appropriate error response
+});
+
+module.exports = router;
