6e003baf4a907cf9190fa1b11d3daf7634576231
Aryan Raj <aryanraj2k25@gmail.com>
2023-07-28 17:16:03 +0530

    Compress Button Loading only after hitting

---
+++ client/src/api/compressHandler.js
@@ -0,0 +1,29 @@
+export async function handleCompress() {
+    try {
+      console.log('Compress button clicked');
+      const response = await fetch('http://localhost:3001/api/compress', {
+        method: 'POST',
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Response:', data);
+        // Assuming the server responds with { parsedData: "..." }
+        if (data.parsedData) {
+          // Successfully received parsed data, return the parsed data
+          return data.parsedData;
+        } else {
+          // Handle the case where the server response doesn't contain the expected data
+          console.error('Error: Invalid Response from the server');
+          return null;
+        }
+      } else {
+        console.error('Error:', response.status);
+        return null;
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  }
+  
--- server/routes/compress.js
+++ server/routes/compress.js
@@ -6,7 +6,10 @@ router.post('/', (req, res) => {
   // Check if media is added and extract text from attached docx or txt file
   // Perform Huffman compression on the text and store it somewhere
   // Respond with success message or appropriate error response
-  console.log('Compression route hit');
+  setTimeout(() => {
+    res.status(200).json({ parsedData: "Compressed data" });
+    console.log('Compression route hit');
+  }, 2000);
 });
 
 router.get('/download', (req, res) => {
